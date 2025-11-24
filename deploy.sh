dfc08ba6c623e012bed336bb4a982b91a5c3690f
David Siaw <874280+davidsiaw@users.noreply.github.com>
2025-11-24 17:15:07 +0900

    first live?

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ rm -rf build/js/MathJax
 # weaver only copies flat files from js/ and css/ so we do a full recursive copy for subdirs
 cp -r js/libs build/js/libs
 cp -r css/libs build/css/libs
+rm -rf build/js/MathJax
 mv gitbak build/.git
 cd build
 echo $FRONTEND_ROOT > CNAME
